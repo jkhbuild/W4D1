ece8e6a708b8513b5ada3e5c71f0b11f6de5b5e5
“Catherine <cathanders10@gmail.com>
2022-10-03 13:53:51 -0400

    valid moves class method

--- Knight_Path_Finder.rb
+++ Knight_Path_Finder.rb
@@ -1,21 +1,30 @@
 require_relative "00_tree_node.rb"
+require "byebug"
 class KnightPathFinder
-    self.valid_moves(pos)
+    def self.valid_moves(pos)
+        valids = [[1,2], [2,1], [-1,2], [-2,1], [1,-2], [2,-1], [-1,-2], [-2,-1]]
         moves = []
+        valids.each do |valid_pos|
+            moves << [(pos[0]+valid_pos[0]),(pos[1]+valid_pos[1])]
+        end
+        moves.select! do |move|
+            ((move[0] < 8) && (move[0] >= 0)) && ((move[1] < 8) && (move[1] >= 0))
 
+        end
         moves
+
     end
+
     attr_reader :start_pos, :root_node
+
     def initialize(start_pos)
         @start_pos = start_pos
         @root_node = PolyTreeNode.new(start_pos)
         @considered_positions = [start_pos]
     end
-
-    # def build_move_tree(root_node)
-
-    # end
 end
 
 kpf = KnightPathFinder.new([0,0])
 p kpf
+
+p KnightPathFinder.valid_moves([3,4])
